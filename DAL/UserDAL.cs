63e8dff045072b65350371a3ff72963de17a0702
NgoTuanAnh <anhnt.114010121001@vtc.edu.vn>
2022-07-18 15:31:10 +0700

    Register function compeleted!

--- DAL/UserDAL.cs
+++ DAL/UserDAL.cs
@@ -49,6 +49,66 @@ public class UserDAL
         return CandidateID;
     }
 
+    public int? InsertNewCandidate(User user)
+    {
+        int? CandidateID = null;
+
+        MySqlCommand cmd = new MySqlCommand("sp_AddCandidate", DBHelper.OpenConnection());
+        try
+        {   
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Username", user.Username);
+            cmd.Parameters["@Username"].Direction = System.Data.ParameterDirection.Input;
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+            cmd.Parameters["@Email"].Direction = System.Data.ParameterDirection.Input;
+            cmd.Parameters.AddWithValue("@UserPassword", user.Password);
+            cmd.Parameters["@UserPassword"].Direction = System.Data.ParameterDirection.Input;
+            cmd.Parameters.AddWithValue("@Gender", user.Gender);
+            cmd.Parameters["@Gender"].Direction = System.Data.ParameterDirection.Input;
+            cmd.Parameters.AddWithValue("@CandidateID", MySqlDbType.Int32);
+            cmd.Parameters["@CandidateID"].Direction = System.Data.ParameterDirection.Output;
+            cmd.ExecuteNonQuery();
+            CandidateID = (int) cmd.Parameters["@CandidateID"].Value;
+        }
+        catch {}
+        finally
+        {
+            DBHelper.CloseConnection();
+        }
+        
+        return CandidateID;
+    }
+
+    public int? InsertNewRecruiter(User user)
+    {
+        int? RecruiterID = null;
+
+        MySqlCommand cmd = new MySqlCommand("sp_AddRecruiter", DBHelper.OpenConnection());
+        try
+        {   
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Username", user.Username);
+            cmd.Parameters["@Username"].Direction = System.Data.ParameterDirection.Input;
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+            cmd.Parameters["@Email"].Direction = System.Data.ParameterDirection.Input;
+            cmd.Parameters.AddWithValue("@UserPassword", user.Password);
+            cmd.Parameters["@UserPassword"].Direction = System.Data.ParameterDirection.Input;
+            cmd.Parameters.AddWithValue("@Gender", user.Gender);
+            cmd.Parameters["@Gender"].Direction = System.Data.ParameterDirection.Input;
+            cmd.Parameters.AddWithValue("@RecruiterID", MySqlDbType.Int32);
+            cmd.Parameters["@RecruiterID"].Direction = System.Data.ParameterDirection.Output;
+            cmd.ExecuteNonQuery();
+            RecruiterID = (int) cmd.Parameters["@RecruiterID"].Value;
+        }
+        catch {}
+        finally
+        {
+            DBHelper.CloseConnection();
+        }
+        
+        return RecruiterID;
+    }
+
     private User GetUserInfo(MySqlDataReader reader)
     {
         User user = new User();
