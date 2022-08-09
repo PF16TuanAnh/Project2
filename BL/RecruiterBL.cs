220d00ff081bb63cf2d4bad49dbb9d5ac0721c10
Ngo Tuan Anh <107117253+PF16TuanAnh@users.noreply.github.com>
2022-08-09 14:20:10 +0700

    updated

--- BL/RecruiterBL.cs
+++ BL/RecruiterBL.cs
@@ -12,25 +12,6 @@ public class RecruiterBL
         recruiterDAL = new RecruiterDAL();
     }
 
-    public List<RecruitNews> GetNewsByProfession(string Profession)
-    {
-        return recruiterDAL.GetNewsByProfession(Profession);
-    }
-
-    public List<RecruitNews> GetNewsBySalaryRange(string SalaryRange)
-    {
-        return recruiterDAL.GetNewsBySalaryRange(SalaryRange);
-    }
-
-    public List<RecruitNews> GetNewsByCityAddress(string CityAddress)
-    {
-        return recruiterDAL.GetNewsByCityAddress(CityAddress);
-    }
-
-    public Recruiter GetRecruiterByNewsID(int NewsID)
-    {
-        return recruiterDAL.GetRecruiterByNewsID(NewsID);
-    }
     // NCT 
     public void CreateNewProfile(Recruiter profile, int? RecruiterID)
     {
